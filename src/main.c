a05366a37af96fa9ca048f3e0adfe46af38cf0f4
hiepdq <dinhquanghiep@gmail.com>
2018-08-24 18:53:02 +0700

    Defeine PIN for LED
    Pin for LED are fixed on Discovery board

--- src/main.c
+++ src/main.c
@@ -1,5 +1,11 @@
 #include <main.h>
 
+/* Config LED pin */
+#define LED_GREEN   GPIO_Pin_12
+#define LED_ORANGE  GPIO_Pin_13
+#define LED_RED     GPIO_Pin_14
+#define LED_BLUE    GPIO_Pin_15
+
 void main(void) {
 
 }
